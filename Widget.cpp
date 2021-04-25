dc2aca629cd62561f2cd14d6f8b37a02d20f7511
Forbinn <vl@vivoka.com>
2021-04-25 10:56:55 +0200

    add(qt, gui): create a basic Qt project

---
+++ Widget.cpp
@@ -0,0 +1,15 @@
+#include "Widget.hpp"
+#include "./ui_Widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
