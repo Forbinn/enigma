ac2fda3241e9d044001068fee3c9b1087f79b8cd
Forbinn <vincent.54.leroy@gmail.com>
2021-04-25 19:40:18 +0200

    update(rotors): the Rotors class is now functional and can have any number of rotor depending on the level of security you want

--- enigma/Rotors.cpp
+++ enigma/Rotors.cpp
@@ -2,6 +2,15 @@
 
 #include "Rotors.hpp"
 
+Enigma::Rotors::Rotors(const string & inputAlphabet,
+                       const string & outputAlphabet)
+    : _inputAlphabet  { inputAlphabet }
+    , _outputAlphabet { outputAlphabet }
+    , _rotors         { { inputAlphabet, outputAlphabet } }
+    , _reflector      { outputAlphabet }
+{
+}
+
 bool Enigma::Rotors::isValid() const
 {
     if (_rotors.empty() || !_reflector.isValid())
@@ -9,6 +18,116 @@ bool Enigma::Rotors::isValid() const
 
     return std::all_of(_rotors.begin(), _rotors.end(),
                        [](const auto & rotor){ return rotor.isValid(); });
+}
+
+void Enigma::Rotors::setInputAlphabet(const string & inputAlphabet)
+{
+    _inputAlphabet = inputAlphabet;
+
+    if (!_rotors.empty())
+        _rotors.front().setInputAlphabet(_inputAlphabet);
+    else if (!_outputAlphabet.empty())
+        _rotors.emplace_back(_inputAlphabet, _outputAlphabet);
+
+    reset();
+}
+
+void Enigma::Rotors::setOutputAlphabet(const string & outputAlphabet)
+{
+    _outputAlphabet = outputAlphabet;
+
+    if (!_rotors.empty())
+        _rotors.back().setOutputAlphabet(_outputAlphabet);
+    else if (!_inputAlphabet.empty())
+        _rotors.emplace_back(_inputAlphabet, _outputAlphabet);
+
+    _reflector.setAlphabet(_outputAlphabet);
+    reset();
+}
+
+void Enigma::Rotors::setIntermediateAlphabets(const std::vector<Enigma::string> & alphabets)
+{
+    if (_rotors.empty())
+        return ;
+
+    clearIntermediateAlphabet();
+
+    auto itr = _rotors.begin();
+    for (const auto & alphabet : alphabets)
+    {
+        _rotors.emplace_back(alphabet, _outputAlphabet);
+        itr->setOutputAlphabet(alphabet);
+        ++itr;
+    }
+}
+
+void Enigma::Rotors::appendIntermediateAlphabet(const string & alphabet)
+{
+    insertIntermediateAlphabet(alphabet, intermediateAlphabetCount());
+}
+
+void Enigma::Rotors::prependIntermediateAlphabet(const string & alphabet)
+{
+    insertIntermediateAlphabet(alphabet, 0);
+}
+
+void Enigma::Rotors::insertIntermediateAlphabet(const string & alphabet, std::size_t index)
+{
+    if (_rotors.empty())
+        return ;
+
+    index = std::clamp(index, {}, intermediateAlphabetCount());
+
+    // The parameter alphabet is used as the input of the new rotor
+    // The output of the previous rotor become the input of the new one
+    // The output of the new rotor become the input of the next rotor
+    // or the global output if it is inserted at the end
+
+    const auto previousRotorItr       = std::next(_rotors.begin(), index);
+    const auto nextRotorItr           = std::next(previousRotorItr);
+    const auto newRotorOutputAlphabet = nextRotorItr == _rotors.end() ? _outputAlphabet
+                                                                      : nextRotorItr->inputAlphabet();
+
+    previousRotorItr->setOutputAlphabet(alphabet);
+    _rotors.emplace(nextRotorItr, alphabet, newRotorOutputAlphabet);
+}
+
+void Enigma::Rotors::removeIntermediateAlphabet(std::size_t index)
+{
+    if (index >= intermediateAlphabetCount())
+        return ;
+
+    const auto currentRotorItr = std::next(_rotors.begin(), index);
+    const auto nextRotorItr    = std::next(currentRotorItr);
+
+    currentRotorItr->setOutputAlphabet(nextRotorItr->outputAlphabet());
+    _rotors.erase(nextRotorItr);
+}
+
+Enigma::string Enigma::Rotors::intermediateAlphabet(std::size_t index) const
+{
+    if (index >= intermediateAlphabetCount())
+        return {};
+
+    return std::next(_rotors.begin(), index)->outputAlphabet();
+}
+
+std::size_t Enigma::Rotors::intermediateAlphabetCount() const
+{
+    if (_rotors.empty())
+        return 0;
+    return _rotors.size() - 1;
+}
+
+std::vector<Enigma::string> Enigma::Rotors::intermediateAlphabets() const
+{
+    if (intermediateAlphabetCount() == 0)
+        return {};
+
+    std::vector<string> alphabets;
+    for (auto itr = std::next(_rotors.begin()); itr != _rotors.end(); ++itr)
+        alphabets.emplace_back(itr->inputAlphabet());
+    return alphabets;
 }
 
 Enigma::value_type Enigma::Rotors::convert(value_type c)
@@ -27,6 +146,24 @@ Enigma::value_type Enigma::Rotors::convert(value_type c)
         c = itr->convertToInput(c);
 
     return c;
+}
+
+void Enigma::Rotors::clear()
+{
+    _inputAlphabet.clear();
+    _outputAlphabet.clear();
+    _rotors.clear();
+    _reflector.clear();
+}
+
+void Enigma::Rotors::clearIntermediateAlphabet()
+{
+    if (intermediateAlphabetCount() == 0)
+        return ;
+
+    for (auto itr = std::next(_rotors.begin()); itr != _rotors.end();)
+        itr = _rotors.erase(itr);
+    _rotors.front().setOutputAlphabet(_outputAlphabet);
 }
 
 void Enigma::Rotors::reset()
--- enigma/Rotors.hpp
+++ enigma/Rotors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <list>
 
 #include "enigma_global.hpp"
 #include "Rotor.hpp"
@@ -11,23 +12,46 @@ namespace Enigma
 class ENIGMA_EXPORT Rotors
 {
 public:
-    using container = std::vector<Rotor>;
+    using container = std::list<Rotor>;
 
 public:
     Rotors() = default;
+    Rotors(const string & inputAlphabet,
+           const string & outputAlphabet);
 
     bool isValid() const;
+
+public:
+    void setInputAlphabet(const string & inputAlphabet);
+    const string & inputAlphabet() const  { return _inputAlphabet; }
+    void setOutputAlphabet(const string & outputAlphabet);
+    const string & outputAlphabet() const { return _outputAlphabet; }
+
+    void setIntermediateAlphabets(const std::vector<string> & alphabets);
+    void appendIntermediateAlphabet(const string & alphabet);
+    void prependIntermediateAlphabet(const string & alphabet);
+    void insertIntermediateAlphabet(const string & alphabet, std::size_t index);
+    void removeIntermediateAlphabet(std::size_t index);
+
+    string intermediateAlphabet(std::size_t index) const;
+    std::size_t intermediateAlphabetCount() const;
+    std::vector<string> intermediateAlphabets() const;
 
 public:
     value_type convert(value_type c);
 
 public:
+    void clear();
+    void clearIntermediateAlphabet();
     void reset();
 
 private:
     void _rotateRotors();
 
 private:
+    string    _inputAlphabet;
+    string    _outputAlphabet;
+
     container _rotors;
     Reflector _reflector;
 };
